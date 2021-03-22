1f135fb49a399ed17ff7068f278b454fac6348cc
Laraib_Saleem <laraibsaleem98@gmail.com>
2021-03-22 23:14:23 +0500

    Errors Fixed in csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,17 +1,17 @@
 import csv
 import datetime
-import models
+import models.item_model
 from database import SessionLocal, engine
 
 db = SessionLocal()
 
-models.Base.metadata.create_all(bind=engine)
+models.item_model.Base.metadata.create_all(bind=engine)
 
 with open("Item.csv", "r") as f:
     csv_reader = csv.DictReader(f)
 
     for row in csv_reader:
-        db_record = models.Item(
+        db_record = models.item_model.Item(
             id=row["id"],
             name=row["name"],
             location=row["location"],
